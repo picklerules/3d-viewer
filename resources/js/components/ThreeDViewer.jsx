283b3170ac8bd8b120ceb2d60672fb43b08afe78
picklerules <136508588+picklerules@users.noreply.github.com>
2024-05-24 10:47:21 -0400

    Add surface area and volume predictions for 3D models

--- resources/js/components/ThreeDViewer.jsx
+++ resources/js/components/ThreeDViewer.jsx
@@ -12,9 +12,9 @@ function calculateMeshProperties(geometry) {
     let totalArea = 0;
     let totalVolume = 0;
     const vertices = geometry.attributes.position.array;
-    const indices = geometry.index ? geometry.index.array : null;
 
-    if (indices) {
+    if (geometry.index) {
+        const indices = geometry.index.array;
         const vA = new THREE.Vector3(), vB = new THREE.Vector3(), vC = new THREE.Vector3();
         const cross = new THREE.Vector3();
 
@@ -29,7 +29,19 @@ function calculateMeshProperties(geometry) {
 
             cross.crossVectors(vB.sub(vA), vC.sub(vA));
             totalArea += cross.length() / 2;
+            totalVolume += vA.dot(cross) / 6;
+        }
+    } else {
+        // Simple estimation for non-indexed meshes
+        // Using each group of nine elements to form an approximate triangle
+        for (let i = 0; i < vertices.length; i += 9) { // Each 9 elements represent 3 points (x, y, z for each point)
+            const vA = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
+            const vB = new THREE.Vector3(vertices[i+3], vertices[i+4], vertices[i+5]);
+            const vC = new THREE.Vector3(vertices[i+6], vertices[i+7], vertices[i+8]);
 
+            const cross = new THREE.Vector3();
+            cross.crossVectors(vB.sub(vA), vC.sub(vA));
+            totalArea += cross.length() / 2;
             totalVolume += vA.dot(cross) / 6;
         }
     }
@@ -39,6 +51,8 @@ function calculateMeshProperties(geometry) {
         volume: Math.abs(totalVolume)
     };
 }
+
+
 
 function ThreeDViewer({ file }) {
     const mountRef = useRef(null);
