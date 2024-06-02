61f4fe9e0807ec320a33f1ae5ef366f8dc8ed4e4
picklerules <136508588+picklerules@users.noreply.github.com>
2024-06-02 15:13:50 -0400

    increase light intensity

--- resources/js/components/ThreeDViewer.jsx
+++ resources/js/components/ThreeDViewer.jsx
@@ -78,13 +78,13 @@ function ThreeDViewer({ file }) {
         mountRef.current.appendChild(renderer.domElement);
 
         // Add lights to the scene
-        const keyLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        const keyLight = new THREE.DirectionalLight(0xffffff, 1.2);
         keyLight.position.set(-100, 0, 100);
-        const fillLight = new THREE.DirectionalLight(0xffffff, 0.75);
+        const fillLight = new THREE.DirectionalLight(0xffffff, 1.0);
         fillLight.position.set(100, 0, 100);
-        const backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        const backLight = new THREE.DirectionalLight(0xffffff, 1.2);
         backLight.position.set(0, 0, -100).normalize();
-        const spotLight = new THREE.SpotLight(0xffffff, 1.5);
+        const spotLight = new THREE.SpotLight(0xffffff, 1.7);
         spotLight.position.set(2, 3, 2);
         spotLight.castShadow = true;
         scene.add(keyLight, fillLight, backLight, spotLight);
